2385b561c21b642bc2a006f6d0e0e55950f9f2e8
jinjaehwan <jjh099@icloud.com>
2023-05-18 14:28:49 +0900

    sdfdsfds

---
+++ frontend/beomflix/src/components/accounts/loginModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <!-- Modal -->
+  <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5 text-black" id="exampleModalLabel">로그인</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body text-dark-emphasis mt-2 mb-2">
+          <form @submit.prevent="login">
+            <label for="username"><b>ID</b></label><br>
+            <input v-model="username" style="width: 100%; padding: 5px;  border-radius: 5px; margin-bottom: 10px;" type="text" name="username" id="username" placeholder="아이디 입력">
+            <label for="password"><b>Password</b></label><br>
+            <input v-model="password" style="width: 100%; padding: 5px;  border-radius: 5px; margin-bottom: 10px;" type="password" name="password" id="password" placeholder="비밀번호 입력">
+            <div class="modal-footer">
+              <input type="submit" class="btn btn-primary" value="로그인">
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:"loginModal",
+  data(){
+    return{
+      username : null,
+      password : null
+    }
+  },
+  methods: {
+    login(){
+      const username = this.username
+      const password = this.password
+
+      const payload = {
+        username, password
+      }
+      this.$store.dispatch('login', payload)
+      this.$router.push({name:"mainView"})
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/beomflix/src/components/common/navbar.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <h1>
+      <a href="">
+          <img style="width:100px; height: 100px;" src="https://github.com/Jinga02/Baekjoon/assets/110621233/f5eecac2-948c-4471-8596-46cad9ee1729" alt="로고">
+      </a>
+    </h1>
+    <div class="menubar">
+      <ul class="menu">
+        <li>영화</li>
+        <li>만화</li>
+        <li>내가 찜한 콘텐츠</li>
+        <input type="text" placeholder="영화 제목을 입력해주세요">
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">내 프로필</a>
+          <ul class="dropdown-menu">
+            <router-link class="dropdown-item" to="/proFile">프로필 보기</router-link>
+            <!-- <li><a class="dropdown-item" href="#scrollspyHeading3">프로필 보기</a></li> -->
+            <li><a class="dropdown-item" href="#scrollspyHeading4" @click="logout">로그아웃</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name : "navBar",
+  methods:{
+    logout(){
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  header{
+    width: 100%;
+    text-align: center;
+    position: relative;
+    height: 120px;
+    border-bottom: 0.1px solid rgb(29, 28, 28);
+  }
+  header h1{
+    position:absolute;   
+    margin: 5px;
+  }
+  
+  header ul.menu{
+    position:absolute;
+    padding:30px 0px;
+    right:20px;
+  }
+  header ul.menu li, header ul.menu input {
+    float: left;
+    padding: 10px 20px;
+    list-style: none;
+    color: white;
+  }
+
+</style>
