e403ac5ff493fcef16d2b0a28885b14123962ccd
김윤욱 <reznov03048@gmail.com>
2023-05-18 14:15:08 +0900

    feat: 영화 전체, 특정 조회

--- backend/beomflix/movies/urls.py
+++ backend/beomflix/movies/urls.py
@@ -5,4 +5,8 @@ from movies import views_tmdb, views
 urlpatterns = [
     # API 데이터 DB 저장
     path('tmdb/', views_tmdb.tmdb_data),
+    # 전체 영화 조회(GET)
+    path('', views.movie_list),
+        # movie_Detail 조회(GET)
+    path('<int:movie_pk>/', views.movie_detail),
 ]
--- backend/beomflix/movies/views.py
+++ backend/beomflix/movies/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework.decorators import permission_classes
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from django.shortcuts import get_list_or_404, get_object_or_404
+from movies.serializers import MovieListSerializer, MovieSerializer
+from movies.models import Movie
+
+
+@api_view(['GET',])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def movie_list(request):
+    if request.method == 'GET':
+        movies = get_list_or_404(Movie.objects.order_by())
+        serializer = MovieListSerializer(movies, many=True)
+        return Response(serializer.data)
+    
+# 영화 디테일 조회(GET)
+@api_view(['GET',])
+@permission_classes([IsAuthenticatedOrReadOnly])
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    # 조회(GET)일 경우
+    if request.method == 'GET':
+        serializer = MovieSerializer(movie)
+        return Response(serializer.data)
