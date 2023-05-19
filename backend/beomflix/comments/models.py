ab3456bbd4aa463d21f51fca829685540c73aea9
김윤욱 <reznov03048@gmail.com>
2023-05-19 15:56:37 +0900

    feat: 유저가 생기면 해당 유저의 프로필이 자동으로 생기도록 구현

--- backend/beomflix/comments/models.py
+++ backend/beomflix/comments/models.py
@@ -1,10 +1,19 @@
 from django.db import models
 from django.conf import settings
+from accounts.models import User
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 # Create your models here.
 class Profile(models.Model):
-    usernumber = models.IntegerField()
+    usernumber = models.OneToOneField(User, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(usernumber=instance)
 
 class Comment(models.Model):
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
