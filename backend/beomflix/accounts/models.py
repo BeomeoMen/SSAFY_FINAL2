9d416cda7aa8f99af5d0437a8cbc5e7873c3b3c7
김윤욱 <reznov03048@gmail.com>
2023-05-22 10:22:45 +0900

    feat: 영화 좋아요, 추천 알고리즘

--- backend/beomflix/accounts/models.py
+++ backend/beomflix/accounts/models.py
@@ -7,3 +7,6 @@ class User(AbstractUser):
     nickname = models.CharField(max_length=20, blank=True)
     profile_path = models.CharField(max_length=10000, blank=True)
     followings = models.ManyToManyField('self', related_name='followers', symmetrical=False)
+
+    def like_movie(self, movie):
+        movie.like_users.add(self)
